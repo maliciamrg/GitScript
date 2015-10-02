9fa15967bacf6ae34aa4a19d0cd57504e0c55f40
david <david@legion-2>
2015-10-03 01:19:27 +0200

    gitallcommitpush

---
+++ gitallpushpull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters (repertoire des git)"
+	exit 0
+fi
+cd $1
+outfile="../gitpushpull-`date +%Y-%m-%d-%H-%M-%S`.txt"
+for f in ./*;
+  do
+	if [ -d $f ]; then
+	cd $f
+	if [ -d .git ]; then
+      echo $f >> $outfile
+	  git config --global push.default matching >> $outfile
+	  git pull >> $outfile
+	  git push >> $outfile
+	fi;
+	cd ..
+	fi;
+  done;
---
+++ gitlistdiff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters (repertoire des git)"
+	exit 0
+fi
+cd $1
+outfile="../gitlistdiff-`date +%Y-%m-%d-%H-%M-%S`.txt"
+for f in ./*;
+  do
+	if [ -d $f ]; then
+	cd $f
+	if [ -d .git ]; then
+	 echo $f >> $outfile 
+	 git show --pretty="format:" >> $outfile
+	 echo $f >> $outfile
+	fi;
+	cd ..
+	fi;
+  done;
